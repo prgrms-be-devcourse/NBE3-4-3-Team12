b3b91a20198a13c7ab4988af9554f074be21bdf7
kjm722 <kjm722@naver.com>
2025-03-04 12:35:46 +0900

    Refactor : GroupResponseDto 코틀린 변환

--- backend/src/main/java/com/example/backend/domain/group/dto/GroupResponseDto.kt
+++ backend/src/main/java/com/example/backend/domain/group/dto/GroupResponseDto.kt
@@ -1,39 +1,32 @@
 package com.example.backend.domain.group.dto;
 
-import com.example.backend.domain.category.dto.CategoryResponseDto;
-import com.example.backend.domain.group.entity.Group;
-import com.example.backend.domain.group.entity.GroupStatus;
-import lombok.Getter;
+import com.example.backend.domain.category.dto.CategoryResponseDto
+import com.example.backend.domain.group.entity.Group
+import com.example.backend.domain.group.entity.GroupStatus
+import java.time.LocalDateTime
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Getter
-public class GroupResponseDto {
-    private Long id;
-    private String title;
-    private String description;
-    private Long memberId;
-    private String author;
-    private Integer maxParticipants;
-    private GroupStatus status;
-    private List<CategoryResponseDto> category;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
-
-    public GroupResponseDto(Group group) {
-        this.id = group.getId();
-        this.title = group.getTitle();
-        this.description = group.getDescription();
-        this.author = group.getMember().getNickname();
-        this.memberId = group.getMember().getId();
-        this.maxParticipants = group.getMaxParticipants();
-        this.status = group.getStatus();
-        this.category = group.getGroupCategories().stream()
-                .map(gc -> new CategoryResponseDto(gc.getCategory()))
-                .collect(Collectors.toList());
-        this.createdAt = group.getCreatedAt();
-        this.modifiedAt = group.getModifiedAt();
-    }
+class GroupResponseDto(
+    val id : Long,
+    val title : String,
+    val description : String,
+    val memberId : Long,
+    val author : String,
+    val maxParticipants : Int,
+    val status : GroupStatus,
+    val category : List<CategoryResponseDto>,
+    val createdAt : LocalDateTime,
+    val modifiedAt : LocalDateTime
+) {
+    constructor(group: Group) : this(
+        id = group.id,
+        title = group.title,
+        description = group.description,
+        memberId = group.member.id!!,
+        author = group.member.nickname,
+        maxParticipants = group.maxParticipants,
+        status = group.status,
+        category = group.groupCategories.map { CategoryResponseDto(it.category) },
+        createdAt = group.createdAt,
+        modifiedAt = group.modifiedAt
+    )
 }
