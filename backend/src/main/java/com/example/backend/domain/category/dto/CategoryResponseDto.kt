7e22f48a5dd6e78d3b2244058daabdf7b5058175
quiemew0 <quiemew@gmail.com>
2025-03-04 15:46:21 +0900

    feat : 카테고리DTO 코틀린변환

--- backend/src/main/java/com/example/backend/domain/category/dto/CategoryResponseDto.kt
+++ backend/src/main/java/com/example/backend/domain/category/dto/CategoryResponseDto.kt
@@ -1,19 +1,16 @@
-package com.example.backend.domain.category.dto;
+package com.example.backend.domain.category.dto
 
-import com.example.backend.domain.category.entity.Category;
-import com.example.backend.domain.category.entity.CategoryType;
-import lombok.Getter;
+import com.example.backend.domain.category.entity.Category
+import com.example.backend.domain.category.entity.CategoryType
 
-@Getter
-public class CategoryResponseDto {
-    private Long id;
-    private CategoryType type;
-    private String name;
-
-
-    public CategoryResponseDto(Category category) {
-        this.id = category.getId();
-        this.type = category.getCategoryType();
-        this.name = category.getName();
-    }
+data class CategoryResponseDto(
+    val id: Long?,
+    val type: CategoryType,
+    val name: String
+) {
+    constructor(category: Category) : this(
+        id = category.id,
+        type = category.categoryType,
+        name = category.name
+    )
 }
