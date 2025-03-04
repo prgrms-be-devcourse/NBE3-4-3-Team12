9d3a47e6fc1479b4efe53a1739c50329ac32ad8b
gorhkd <gorhkd223@gmail.com>
2025-03-04 22:02:25 +0900

    Refactor: Admin DTO 코틀린 변환

--- backend/src/main/java/com/example/backend/domain/admin/dto/AdminLoginRequest.kt
+++ backend/src/main/java/com/example/backend/domain/admin/dto/AdminLoginRequest.kt
@@ -1,9 +1,11 @@
 package com.example.backend.domain.admin.dto;
 
-import lombok.Getter;
+import jakarta.validation.constraints.NotBlank
 
-@Getter
-public class AdminLoginRequest {
-    private String adminName;
-    private String password;
-}
+data class AdminLoginRequest (
+    @NotBlank(message = "아이디를 적어주세요")
+    val adminName: String,
+
+    @NotBlank(message = "비밀번호를 적어주세요")
+    val password: String
+)
