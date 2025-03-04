ce6cea78e383a8282668e54ec68158c89afd85a4
kjm722 <kjm722@naver.com>
2025-03-04 13:47:06 +0900

    Refactor : GroupMember 엔티티 Builder 제거

--- backend/src/main/java/com/example/backend/domain/groupmember/entity/GroupMember.java
+++ backend/src/main/java/com/example/backend/domain/groupmember/entity/GroupMember.java
@@ -4,7 +4,6 @@ import com.example.backend.domain.group.entity.Group;
 import com.example.backend.domain.member.entity.Member;
 import com.example.backend.global.base.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,10 +28,14 @@ public class GroupMember extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     private GroupMemberStatus groupMemberStatus;
 
-    @Builder
     public GroupMember(Member member, Group group, GroupMemberStatus groupMemberStatus) {
         this.member = member;
         this.group = group;
         this.groupMemberStatus = GroupMemberStatus.APPLYING;
+    }
+
+    public GroupMember(Member member, Group group) {
+        this.member = member;
+        this.group = group;
     }
 }
