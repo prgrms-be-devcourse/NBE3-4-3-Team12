975128550ac80ae5ac1550c06a4a809f7979b6c2
kjm722 <kjm722@naver.com>
2025-03-04 12:15:35 +0900

    Refactor : GroupModifyRequestDto 코틀린 변환

--- backend/src/main/java/com/example/backend/domain/group/dto/GroupModifyRequestDto.kt
+++ backend/src/main/java/com/example/backend/domain/group/dto/GroupModifyRequestDto.kt
@@ -1,21 +1,16 @@
-package com.example.backend.domain.group.dto;
+package com.example.backend.domain.group.dto
 
-import com.example.backend.domain.group.entity.GroupStatus;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.example.backend.domain.group.entity.GroupStatus
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-
-@Getter
-@AllArgsConstructor
-public class GroupModifyRequestDto {
-    @NotBlank(message = "타이틀은 필수 항목입니다.")
-    private String title;
-    @NotBlank(message = "설명은 필수 항목입니다.")
-    private String description;
-    @NotNull(message = "인원은 필수 항목입니다.")
-    private Integer maxParticipants;
-    @NotNull(message = "모집상태는 필수 항목입니다.")
-    private GroupStatus groupStatus;
-}
+data class GroupModifyRequestDto(
+    @field:NotBlank(message = "타이틀은 필수 항목입니다.")
+    val title: String,
+    @field:NotBlank(message = "설명은 필수 항목입니다.")
+    val description: String,
+    @field:NotNull(message = "인원은 필수 항목입니다.")
+    val maxParticipants: Int,
+    @field:NotNull(message = "모집상태는 필수 항목입니다.")
+    val groupStatus: GroupStatus
+)
