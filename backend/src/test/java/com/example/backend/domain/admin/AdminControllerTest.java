3a911ffb40ccb85690cab516fbfef3c20527a1a6
gorhkd <gorhkd223@gmail.com>
2025-03-04 22:03:53 +0900

    Refactor: admin test 수정

--- backend/src/test/java/com/example/backend/domain/admin/AdminControllerTest.java
+++ backend/src/test/java/com/example/backend/domain/admin/AdminControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Transactional
 public class AdminControllerTest {
 
@@ -129,7 +131,7 @@ public class AdminControllerTest {
                 .cookie(new Cookie("refreshToken", refreshToken)));
 
         deleteGroupResponse
-                .andExpect(status().isOk());
+                .andExpect(status().isNoContent());
 
         Group group = this.groupRepository.findById(groupId).orElseThrow();
         assertThat(group.getStatus()).isEqualTo(DELETED);
