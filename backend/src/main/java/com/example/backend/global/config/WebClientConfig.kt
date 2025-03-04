778680af2ba4715b61b5c1c197c6c616bfaa116e
100gammmit <100minha@naver.com>
2025-03-04 15:49:01 +0900

    Refactor: WebClientConfig 설정 클래스 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/config/WebClientConfig.kt
+++ backend/src/main/java/com/example/backend/global/config/WebClientConfig.kt
@@ -1,8 +1,8 @@
-package com.example.backend.global.config;
+package com.example.backend.global.config
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
 
 /**
  * WebClientConfig
@@ -10,10 +10,9 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @author 100minha
  */
 @Configuration
-public class WebClientConfig {
-
-	@Bean
-	public WebClient webClient() {
-		return WebClient.builder().build();
-	}
+class WebClientConfig {
+    @Bean
+    fun webClient(): WebClient {
+        return WebClient.builder().build()
+    }
 }
