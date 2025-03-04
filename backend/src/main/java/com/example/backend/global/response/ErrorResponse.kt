2c1efd74f4be270578016f68679e35738ce61866
100gammmit <100minha@naver.com>
2025-03-04 15:47:14 +0900

    Refactor: ErrorResponse 클래스 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/response/ErrorResponse.kt
+++ backend/src/main/java/com/example/backend/global/response/ErrorResponse.kt
@@ -1,61 +1,57 @@
-package com.example.backend.global.response;
+package com.example.backend.global.response
 
-import lombok.AccessLevel;
-import lombok.Builder;
-
-import java.util.List;
+import lombok.AccessLevel
+import lombok.Builder
 
 /**
  * ErrorResponse
  * 전역에서 예외 발생 시 응답떄 사용할 클래스
  * @author 100minha
  */
-@Builder(access = AccessLevel.PRIVATE)
-public record ErrorResponse(
-	String message,
-	String code,
-	String requestUri,
-	List<ValidationError> errors
+data class ErrorResponse(
+    val message: String,
+    val code: String,
+    val requestUri: String,
+    val errors: List<ValidationError>? = null
 ) {
+    /**
+     * Validation 예외 발생 시 필드별 예외 정보 관리 클래스
+     * @param field
+     * @param message
+     */
+    data class ValidationError(
+        val field: String,  // 에러가 발생한 필드명
+        val message: String? = "" // 해당 필드의 에러 메시지
+    )
 
-	/**
-	 * Validation 예외 발생 시 필드별 예외 정보 관리 클래스
-	 * @param field
-	 * @param message
-	 */
-	@Builder
-	public static record ValidationError(
-		String field,    // 에러가 발생한 필드명
-		String message    // 해당 필드의 에러 메시지
-	) {
-	}
+    companion object {
+        /**
+         * Validation 예외 발생 시 응답 때 사용할 팩토리 메서드
+         * @param message    // 예외 메세지
+         * @param code    // 예외 상태 코드
+         * @param errors    // 필드별 에러 메세지
+         */
+        fun of(message: String, code: String, requestUri: String, errors: List<ValidationError>): ErrorResponse {
+            return ErrorResponse(
+                message = message,
+                code = code,
+                requestUri = requestUri,
+                errors = errors
+            )
+        }
 
-	/**
-	 * Validation 예외 발생 시 응답 때 사용할 팩토리 메서드
-	 * @param message    // 예외 메세지
-	 * @param code    // 예외 상태 코드
-	 * @param errors    // 필드별 에러 메세지
-	 */
-	public static ErrorResponse of(String message, String code, String requestUri, List<ValidationError> errors) {
-		return ErrorResponse.builder()
-			.message(message)
-			.code(code)
-			.requestUri(requestUri)
-			.errors(errors)
-			.build();
-	}
-
-	/**
-	 * 이외에 FieldError를 포함하지 않는 예외 발생 시 사용할 팩토리 메서드
-	 * @param message    // 예외 메세지
-	 * @param code    // 예외 상태 코드
-	 */
-	public static ErrorResponse of(String message, String code, String requestUri) {
-		return ErrorResponse.builder()
-			.message(message)
-			.code(code)
-			.requestUri(requestUri)
-			.build();
-	}
-
+        /**
+         * 이외에 FieldError를 포함하지 않는 예외 발생 시 사용할 팩토리 메서드
+         * @param message    // 예외 메세지
+         * @param code    // 예외 상태 코드
+         */
+        @JvmStatic
+		fun of(message: String, code: String, requestUri: String): ErrorResponse {
+            return ErrorResponse(
+                message = message,
+                code = code,
+                requestUri = requestUri
+            )
+        }
+    }
 }
