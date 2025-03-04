9802e8a7f255f8b77a061d70ae2f9e531c969647
100gammmit <100minha@naver.com>
2025-03-04 15:48:15 +0900

    Refactor: Global 공통 커스텀 예외 클래스 코틀린 리팩토링 (#3)
    - 추후 '!!' 연산자 삭제 필요

--- backend/src/main/java/com/example/backend/global/exception/GlobalErrorCode.kt
+++ backend/src/main/java/com/example/backend/global/exception/GlobalErrorCode.kt
@@ -1,22 +1,20 @@
-package com.example.backend.global.exception;
+package com.example.backend.global.exception
 
-import org.springframework.http.HttpStatus;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.AllArgsConstructor
+import lombok.Getter
+import org.springframework.http.HttpStatus
 
 /**
  * GlobalErrorCode
  * 전역에서 발생 할 수 있는 커스텀 예외 정리 클래스
  * @author 100minha
  */
-@AllArgsConstructor
-@Getter
-public enum GlobalErrorCode {
-	NOT_VALID(HttpStatus.BAD_REQUEST, "400-1", "입력된 객체가 유효하지 않습니다");
 
-	private final HttpStatus httpStatus;
-	private final String code;
-	private final String message;
+enum class GlobalErrorCode(
+    val httpStatus: HttpStatus,
+    val code: String,
+    val message: String
+) {
+    NOT_VALID(HttpStatus.BAD_REQUEST, "400-1", "입력된 객체가 유효하지 않습니다");
 
 }
--- backend/src/main/java/com/example/backend/global/exception/GlobalException.kt
+++ backend/src/main/java/com/example/backend/global/exception/GlobalException.kt
@@ -1,25 +1,21 @@
-package com.example.backend.global.exception;
+package com.example.backend.global.exception
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus
 
 /**
  * GlobalException
  * 전역에서 발생할 수 있는 커스텀 예외 처리 클래스
  * @author 100minha
  */
-public class GlobalException extends RuntimeException {
-	private final GlobalErrorCode globalErrorCode;
+class GlobalException(private val globalErrorCode: GlobalErrorCode) :
+    RuntimeException() {
 
-	public GlobalException(GlobalErrorCode globalErrorCode) {
-		super(globalErrorCode.getMessage());
-		this.globalErrorCode = globalErrorCode;
-	}
+    override val message: String
+        get() = globalErrorCode.message
 
-	public HttpStatus getStatus() {
-		return globalErrorCode.getHttpStatus();
-	}
+    val status: HttpStatus
+        get() = globalErrorCode.httpStatus
 
-	public String getCode() {
-		return globalErrorCode.getCode();
-	}
+    val code: String
+        get() = globalErrorCode.code
 }
