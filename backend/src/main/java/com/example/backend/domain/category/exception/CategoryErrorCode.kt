9e062d94a21c870fe5df9c4608632c745ef75c0e
quiemew0 <quiemew@gmail.com>
2025-03-04 15:47:46 +0900

    refactor : 카테고리 예외처리 코틀린변환

--- backend/src/main/java/com/example/backend/domain/category/exception/CategoryErrorCode.kt
+++ backend/src/main/java/com/example/backend/domain/category/exception/CategoryErrorCode.kt
@@ -1,16 +1,12 @@
-package com.example.backend.domain.category.exception;
+package com.example.backend.domain.category.exception
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus
 
-@AllArgsConstructor
-@Getter
-public enum CategoryErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND,"404","해당 카테고리는 존재하지 않습니다."),
-    NOT_FOUND_LIST(HttpStatus.NOT_FOUND,"404","카테고리 목록이 존재하지 않습니다.");
-
-    private final HttpStatus httpStatus;
-    private final String code;
-    private final String message;
+enum class CategoryErrorCode(
+    val httpStatus: HttpStatus,
+    val code: String,
+    val message: String
+) {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "404", "해당 카테고리는 존재하지 않습니다."),
+    NOT_FOUND_LIST(HttpStatus.NOT_FOUND, "404", "카테고리 목록이 존재하지 않습니다.")
 }
