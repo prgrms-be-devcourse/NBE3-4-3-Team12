63e70c2fbf7c4d73f3f7bc848c1f338bd30aafb0
100gammmit <100minha@naver.com>
2025-03-04 15:47:53 +0900

    Refactor: GlobalControllerAdvise 코틀린 리팩토링 (#3)
    - 추후 '!!' 연산자 삭제 필요

--- backend/src/main/java/com/example/backend/global/advice/GlobalControllerAdvice.kt
+++ backend/src/main/java/com/example/backend/global/advice/GlobalControllerAdvice.kt
@@ -1,121 +1,126 @@
-package com.example.backend.global.advice;
+package com.example.backend.global.advice
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.example.backend.domain.admin.exception.AdminException;
-import com.example.backend.domain.category.exception.CategoryException;
-import com.example.backend.domain.group.exception.GroupException;
-import com.example.backend.domain.member.exception.MemberException;
-import com.example.backend.domain.voter.exception.VoterException;
-import com.example.backend.global.exception.GlobalErrorCode;
-import com.example.backend.global.exception.GlobalException;
-import com.example.backend.global.response.ErrorResponse;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
+import com.example.backend.domain.admin.exception.AdminException
+import com.example.backend.domain.category.exception.CategoryException
+import com.example.backend.domain.group.exception.GroupException
+import com.example.backend.domain.member.exception.MemberException
+import com.example.backend.domain.voter.exception.VoterException
+import com.example.backend.global.exception.GlobalErrorCode
+import com.example.backend.global.exception.GlobalException
+import com.example.backend.global.response.ErrorResponse
+import com.example.backend.global.response.ErrorResponse.Companion.of
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.ConstraintViolation
+import jakarta.validation.ConstraintViolationException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.util.function.Consumer
 
 /**
  * GlobalControllerAdvice
  * Global 예외 처리 advice 클래스
  * @author 100minha
  */
+// TODO: !! 연산자 사용 제거
 @ControllerAdvice
-public class GlobalControllerAdvice {
+class GlobalControllerAdvice {
+    /**
+     * 컨트롤러 @Valid 검증 실패 예외 핸들러 메소드
+     * 컨트롤러 레이어에서
+     */
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(
+        ex: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        val errors: MutableList<ErrorResponse.ValidationError> = ArrayList()
 
-	/**
-	 * 컨트롤러 @Valid 검증 실패 예외 핸들러 메소드
-	 * 컨트롤러 레이어에서
-	 */
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-		HttpServletRequest request) {
-		List<ErrorResponse.ValidationError> errors = new ArrayList<>();
+        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
+            errors.add(
+                ErrorResponse.ValidationError(
+                    (error as FieldError).field,
+                    error.getDefaultMessage()
+                )
+            )
+        })
 
-		ex.getBindingResult().getAllErrors().forEach(error -> {
-			errors.add(ErrorResponse.ValidationError.builder()
-				.field(((FieldError)error).getField())
-				.message(error.getDefaultMessage())
-				.build());
-		});
+        return ResponseEntity.status(ex.statusCode.value()).body(
+            ErrorResponse.of(
+                GlobalErrorCode.NOT_VALID.message,
+                GlobalErrorCode.NOT_VALID.code,
+                request.requestURI,
+                errors
+            )
+        )
+    }
 
-		return ResponseEntity.status(ex.getStatusCode().value()).body(
-			ErrorResponse.of(
-				GlobalErrorCode.NOT_VALID.getMessage(),
-				GlobalErrorCode.NOT_VALID.getCode(),
-				request.getRequestURI(),
-				errors
-			)
-		);
-	}
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(
+        ex: ConstraintViolationException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        val errors: MutableList<ErrorResponse.ValidationError> = ArrayList()
 
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex,
-		HttpServletRequest request) {
-		List<ErrorResponse.ValidationError> errors = new ArrayList<>();
+        ex.constraintViolations.forEach(Consumer { violation: ConstraintViolation<*> ->
+            val fieldName = violation.propertyPath.toString()
+            val message = violation.message
+            errors.add(ErrorResponse.ValidationError(fieldName, message))
+        })
 
-		ex.getConstraintViolations().forEach(violation -> {
-			String fieldName = violation.getPropertyPath().toString();
-			String message = violation.getMessage();
-			errors.add(new ErrorResponse.ValidationError(fieldName, message));
-		});
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ErrorResponse.of(
+                GlobalErrorCode.NOT_VALID.message,
+                GlobalErrorCode.NOT_VALID.code,
+                request.requestURI,
+                errors
+            )
+        )
+    }
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-			ErrorResponse.of(
-				GlobalErrorCode.NOT_VALID.getMessage(),
-				GlobalErrorCode.NOT_VALID.getCode(),
-				request.getRequestURI(),
-				errors
-			)
-		);
-	}
+    @ExceptionHandler(GroupException::class)
+    fun handleGroupException(ex: GroupException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message!!, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(GroupException.class)
-	public ResponseEntity<ErrorResponse> handleGroupException(GroupException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
+    @ExceptionHandler(GlobalException::class)
+    fun handleGlobalException(ex: GlobalException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(GlobalException.class)
-	public ResponseEntity<ErrorResponse> handleGlobalException(GlobalException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
+    @ExceptionHandler(AdminException::class)
+    fun handleAdminException(ex: AdminException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message!!, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(AdminException.class)
-	public ResponseEntity<ErrorResponse> handleAdminException(AdminException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
+    @ExceptionHandler(CategoryException::class)
+    fun handleCategoryException(ex: CategoryException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message!!, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(CategoryException.class)
-	public ResponseEntity<ErrorResponse> handleCategoryException(CategoryException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
+    @ExceptionHandler(MemberException::class)
+    fun handleMemberException(ex: MemberException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(MemberException.class)
-	public ResponseEntity<ErrorResponse> handleMemberException(MemberException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
+    @ExceptionHandler(VoterException::class)
+    fun handleVoterException(ex: VoterException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(ex.status)
+            .body(ErrorResponse.of(ex.message!!, ex.code, request.requestURI))
+    }
 
-	@ExceptionHandler(VoterException.class)
-	public ResponseEntity<ErrorResponse> handleVoterException(VoterException ex, HttpServletRequest request) {
-		return ResponseEntity.status(ex.getStatus())
-			.body(ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI()));
-	}
-
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleVoterException(Exception ex, HttpServletRequest request) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(ErrorResponse.of(ex.getMessage(), "500", request.getRequestURI()));
-	}
+    @ExceptionHandler(Exception::class)
+    fun handleVoterException(ex: Exception, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorResponse.of(ex.message ?: "INTERNAL_SERVER_ERROR", "500", request.requestURI))
+    }
 }
