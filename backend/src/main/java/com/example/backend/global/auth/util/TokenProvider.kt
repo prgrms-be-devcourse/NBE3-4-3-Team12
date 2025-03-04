8eb5bf78090bb4900043f3f2ebecc83e1657c1a1
gorhkd <gorhkd223@gmail.com>
2025-03-04 22:03:17 +0900

    Refactor: TokenProvider 코틀린 변환

--- backend/src/main/java/com/example/backend/global/auth/util/TokenProvider.kt
+++ backend/src/main/java/com/example/backend/global/auth/util/TokenProvider.kt
@@ -1,52 +1,47 @@
 package com.example.backend.global.auth.util;
 
-import com.example.backend.domain.admin.entity.Admin;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.UUID;
+import com.example.backend.domain.admin.entity.Admin
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.LocalDateTime
+import java.util.*
 
 @Component
-@RequiredArgsConstructor
-public class TokenProvider {
-
-	private final JwtUtil jwtUtil;
+class TokenProvider(
+	private val jwtUtil: JwtUtil
+) {
 
 	// JWT 토큰 생성
-	public String generateToken(Admin admin) {
+	fun generateToken(admin: Admin): String {
 		return Jwts.builder()
-			.setSubject(admin.getId().toString())  // 사용자 이름 (관리자 ID)
-			.claim("role", admin.getRole())  // 역할 (권한 정보)
-			.setIssuedAt(new Date())  // 토큰 발급 시간
-			.setExpiration(new Date(System.currentTimeMillis() + jwtUtil.getAccessTokenExpirationTime()))  // 만료시간
+			.setSubject(admin.id.toString())  // 사용자 이름 (관리자 ID)
+			.claim("role", admin.role)  // 역할 (권한 정보)
+			.setIssuedAt(Date())  // 토큰 발급 시간
+			.setExpiration(Date(System.currentTimeMillis() + jwtUtil.getAccessTokenExpirationTime()))  // 만료시간
 			.signWith(jwtUtil.getKey())  // 서명 알고리즘 및 키
-			.compact();
+			.compact()
 	}
 
-	public String generateMemberAccessToken(Long id, String nickname, String email) {
+	fun generateMemberAccessToken(id: Long, nickname: String, email: String): String {
 		return Jwts.builder()
 			.setSubject(nickname)  // 사용자 nickname
 			.claim("id", id)    // Member primary-key
 			.claim("email", email)
-			.setIssuedAt(new Date())  // 토큰 발급 시간
-			.setExpiration(new Date(System.currentTimeMillis() + jwtUtil.getAccessTokenExpirationTime()))  // 만료시간
+			.setIssuedAt(Date())  // 토큰 발급 시간
+			.setExpiration(Date(System.currentTimeMillis() + jwtUtil.getAccessTokenExpirationTime()))  // 만료시간
 			.signWith(jwtUtil.getKey(), SignatureAlgorithm.HS256)  // 서명 알고리즘 및 키
-			.compact();
+			.compact()
 	}
 
 	// 리프레시 토큰 생성
-	public String generateRefreshToken() {
-		return UUID.randomUUID().toString();
+	fun generateRefreshToken(): String {
+		return UUID.randomUUID().toString()
 	}
 
 	// 리프레시 토큰 만료 시간 계산
-	public LocalDateTime getRefreshTokenExpiryDate() {
-		return LocalDateTime.now().plus(Duration.ofMillis(jwtUtil.getRefreshTokenExpirationTime()));
+	fun getRefreshTokenExpiryDate(): LocalDateTime {
+		return LocalDateTime.now().plus(Duration.ofMillis(jwtUtil.getRefreshTokenExpirationTime()))
 	}
-
 }
