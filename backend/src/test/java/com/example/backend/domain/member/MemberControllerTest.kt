31503bb634e81df11980993a675b4d948f3d97dd
100gammmit <100minha@naver.com>
2025-03-05 21:45:05 +0900

    Refactor: Test코드 코틀린 리팩토링 (#3)

--- backend/src/test/java/com/example/backend/domain/member/MemberControllerTest.kt
+++ backend/src/test/java/com/example/backend/domain/member/MemberControllerTest.kt
@@ -1,190 +1,207 @@
-package com.example.backend.domain.member;
+package com.example.backend.domain.member
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.backend.domain.member.controller.MemberController;
-import com.example.backend.domain.member.entity.Member;
-import com.example.backend.domain.member.repository.MemberRepository;
-import com.example.backend.domain.member.service.MemberService;
-import com.example.backend.global.util.TestTokenProvider;
-
-import jakarta.servlet.http.Cookie;
+import com.example.backend.domain.member.controller.MemberController
+import com.example.backend.domain.member.entity.Member
+import com.example.backend.domain.member.repository.MemberRepository
+import com.example.backend.global.util.TestTokenProvider
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import jakarta.servlet.http.Cookie
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * MemberControllerTest
- * <p></p>
+ *
+ *
  * @author 100minha
  */
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
-public class MemberControllerTest {
+class MemberControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
-	@Autowired
-	private MemberService memberService;
+    @Autowired
+    private lateinit var tokenProvider: TestTokenProvider
 
-	/*
-	테스트용 사용자 정보를 쿠키에서 조회하기 위한 데이터 설정
-	테스트용 엑세스토큰까지 생성 및 api 요청 시 쿠키에 담아서 요청해야함
-	 */
-	private static String accessToken;
+    @Autowired
+    private lateinit var memberRepository: MemberRepository
 
-	@BeforeAll
-	static void setUp(@Autowired TestTokenProvider tokenProvider, @Autowired MemberRepository memberRepository) {
-		Member member = new Member(1L, "testUser", "test@test.com");
+    @PersistenceContext
+    private lateinit var em: EntityManager
 
-		memberRepository.save(member);
+    private lateinit var accessToken: String
 
-		accessToken = tokenProvider.generateMemberAccessToken(
-			member.getId(), member.getNickname(), member.getEmail());
-	}
+    @BeforeEach
+    fun setUp() {
+        memberRepository.deleteAll()
+        em.createNativeQuery("ALTER TABLE member ALTER COLUMN id RESTART WITH 1").executeUpdate()
 
-	@Test
-	@DisplayName("로그인된 사용자 정보 조회 테스트")
-	void getCurrentMemberTest() throws Exception {
-		// given
-		Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        val member = Member(1L, "testUser", "test@test.com")
+        memberRepository.save(member)
 
-		// when
-		ResultActions resultActions = mockMvc.perform(get("/members")
-			.cookie(accessTokenCookie));
+        accessToken = tokenProvider.generateMemberAccessToken(
+            member.id!!, member.nickname, member.email
+        )
+    }
 
-		//then
-		resultActions
-			.andExpect(handler().handlerType(MemberController.class))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.data.id").value(1))
-			.andExpect(jsonPath("$.data.nickname").value("testUser"))
-			.andExpect(jsonPath("$.data.email").value("test@test.com"));
-	}
+    @Test
+    @DisplayName("로그인된 사용자 정보 조회 테스트")
+    fun getCurrentMemberTest() {
+        // given
+        val accessTokenCookie = Cookie("accessToken", accessToken)
 
-	@Test
-	@DisplayName("로그인 되지 않은 사용자 정보 조회 실패 테스트")
-	void getCurrentMemberWhenNoCookieTest() throws Exception {
-		// given
+        // when
+        val resultActions = mockMvc.perform(get("/members").cookie(accessTokenCookie))
 
-		// when
-		ResultActions resultActions = mockMvc.perform(get("/members"));
+        // then
+        resultActions
+            .andExpect(handler().handlerType(MemberController::class.java))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.data.id").value(1))
+            .andExpect(jsonPath("$.data.nickname").value("testUser"))
+            .andExpect(jsonPath("$.data.email").value("test@test.com"))
+    }
 
-		//then
-		resultActions
-			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.message").value("인증에 실패했습니다."))
-			.andExpect(jsonPath("$.code").value("401-1"));
-	}
+    @Test
+    @DisplayName("로그인 되지 않은 사용자 정보 조회 실패 테스트")
+    fun getCurrentMemberWhenNoCookieTest() {
+        // when
+        val resultActions = mockMvc.perform(get("/members"))
 
-	@Test
-	@DisplayName("사용자 정보 수정 성공 테스트")
-	void modifyTest() throws Exception {
-		// given
-		Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        // then
+        resultActions
+            .andExpect(status().isUnauthorized)
+            .andExpect(jsonPath("$.message").value("인증에 실패했습니다."))
+            .andExpect(jsonPath("$.code").value("401-1"))
+    }
 
-		// when
-		ResultActions resultActions = mockMvc.perform(put("/members")
-			.content("""
-				{
-					"nickname" : "modified"
-				}
-				""")
-			.contentType(MediaType.APPLICATION_JSON)
-			.cookie(accessTokenCookie));
+    @Test
+    @DisplayName("사용자 정보 수정 성공 테스트")
+    fun modifyTest() {
+        // given
+        val accessTokenCookie = Cookie("accessToken", accessToken)
 
-		//then
-		resultActions
-			.andExpect(handler().handlerType(MemberController.class))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.data.id").value(1))
-			.andExpect(jsonPath("$.data.nickname").value("modified"))
-			.andExpect(jsonPath("$.data.email").value("test@test.com"));
-	}
+        // when
+        val resultActions = mockMvc.perform(
+            put("/members")
+                .content(
+                    """
+                    {
+                        "nickname" : "modified"
+                    }
+                    """.trimIndent()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie(accessTokenCookie)
+        )
 
-	@Test
-	@DisplayName("사용자 정보 수정 실패(nickname 공백) 테스트")
-	void modifyFailWhenBlankNicknameTest() throws Exception {
-		// given
-		Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        // then
+        resultActions
+            .andExpect(handler().handlerType(MemberController::class.java))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.data.id").value(1))
+            .andExpect(jsonPath("$.data.nickname").value("modified"))
+            .andExpect(jsonPath("$.data.email").value("test@test.com"))
+    }
 
-		// when
-		ResultActions resultActions = mockMvc.perform(put("/members")
-			.content("""
-				{
-					"nickname" : ""
-				}
-				""")
-			.contentType(MediaType.APPLICATION_JSON)
-			.cookie(accessTokenCookie));
+    @Test
+    @DisplayName("사용자 정보 수정 실패(nickname 공백) 테스트")
+    fun modifyFailWhenBlankNicknameTest() {
+        // given
+        val accessTokenCookie = Cookie("accessToken", accessToken)
 
-		//then
-		resultActions
-			.andExpect(handler().handlerType(MemberController.class))
-			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.message").value("입력된 객체가 유효하지 않습니다"))
-			.andExpect(jsonPath("$.code").value("400-1"))
-			.andExpect(jsonPath("$.errors[0].field").value("nickname"))
-			.andExpect(jsonPath("$.errors[0].message").value("닉네임은 최소 2글자 이상, 10글자 이하이어야 합니다."));
-	}
+        // when
+        val resultActions = mockMvc.perform(
+            put("/members")
+                .content(
+                    """
+                    {
+                        "nickname" : ""
+                    }
+                    """.trimIndent()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie(accessTokenCookie)
+        )
 
-	@Test
-	@DisplayName("사용자 정보 수정 실패(nickname 길이 최대치 초과) 테스트")
-	void modifyFailWhenTooLongNicknameTest() throws Exception {
-		// given
-		Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        // then
+        resultActions
+            .andExpect(handler().handlerType(MemberController::class.java))
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.message").value("입력된 객체가 유효하지 않습니다"))
+            .andExpect(jsonPath("$.code").value("400-1"))
+            .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+            .andExpect(jsonPath("$.errors[0].message").value("닉네임은 최소 2글자 이상, 10글자 이하이어야 합니다."))
+    }
 
-		// when
-		ResultActions resultActions = mockMvc.perform(put("/members")
-			.content("""
-				{
-					"nickname" : "too_Long_Nickname"
-				}
-				""")
-			.contentType(MediaType.APPLICATION_JSON)
-			.cookie(accessTokenCookie));
+    @Test
+    @DisplayName("사용자 정보 수정 실패(nickname 길이 최대치 초과) 테스트")
+    fun modifyFailWhenTooLongNicknameTest() {
+        // given
+        val accessTokenCookie = Cookie("accessToken", accessToken)
 
-		//then
-		resultActions
-			.andExpect(handler().handlerType(MemberController.class))
-			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.message").value("입력된 객체가 유효하지 않습니다"))
-			.andExpect(jsonPath("$.code").value("400-1"))
-			.andExpect(jsonPath("$.errors[0].field").value("nickname"))
-			.andExpect(jsonPath("$.errors[0].message").value("닉네임은 최소 2글자 이상, 10글자 이하이어야 합니다."));
-	}
+        // when
+        val resultActions = mockMvc.perform(
+            put("/members")
+                .content(
+                    """
+                    {
+                        "nickname" : "too_Long_Nickname"
+                    }
+                    """.trimIndent()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie(accessTokenCookie)
+        )
 
-	@Test
-	@DisplayName("사용자 정보 수정 실패(로그인되지 않은 상태) 테스트")
-	void modifyFailWhenNoCookieTest() throws Exception {
-		// given
+        // then
+        resultActions
+            .andExpect(handler().handlerType(MemberController::class.java))
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.message").value("입력된 객체가 유효하지 않습니다"))
+            .andExpect(jsonPath("$.code").value("400-1"))
+            .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+            .andExpect(jsonPath("$.errors[0].message").value("닉네임은 최소 2글자 이상, 10글자 이하이어야 합니다."))
+    }
 
-		// when
-		ResultActions resultActions = mockMvc.perform(put("/members")
-			.content("""
-				{
-					"nickname" : "modified"
-				}
-				""")
-			.contentType(MediaType.APPLICATION_JSON));
+    @Test
+    @DisplayName("사용자 정보 수정 실패(로그인되지 않은 상태) 테스트")
+    fun modifyFailWhenNoCookieTest() {
+        // when
+        val resultActions = mockMvc.perform(
+            put("/members")
+                .content(
+                    """
+                    {
+                        "nickname" : "modified"
+                    }
+                    """.trimIndent()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+        )
 
-		//then
-		resultActions
-			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.message").value("인증에 실패했습니다."))
-			.andExpect(jsonPath("$.code").value("401-1"));
-	}
+        // then
+        resultActions
+            .andExpect(status().isUnauthorized)
+            .andExpect(jsonPath("$.message").value("인증에 실패했습니다."))
+            .andExpect(jsonPath("$.code").value("401-1"))
+    }
 }
+
