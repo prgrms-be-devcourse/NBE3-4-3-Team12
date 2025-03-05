25ce7cba773d0e87a5ac40387e7cafd9c58c4699
100gammmit <100minha@naver.com>
2025-03-05 17:27:32 +0900

    Refactor: CookieUtil 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/auth/util/CookieUtil.kt
+++ backend/src/main/java/com/example/backend/global/auth/util/CookieUtil.kt
@@ -1,13 +1,11 @@
-package com.example.backend.global.auth.util;
+package com.example.backend.global.auth.util
 
-import java.util.Arrays;
-
-import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Component;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.ResponseCookie
+import org.springframework.stereotype.Component
+import java.util.*
 
 /**
  * CookieUtil
@@ -15,42 +13,35 @@ import jakarta.servlet.http.HttpServletResponse;
  * @author 100minha
  */
 @Component
-public class CookieUtil {
+class CookieUtil {
+    /**
+     * 쿠키에 name의 토큰을 저장하는 메서드
+     * @param name
+     * @param value
+     * @param expiration
+     * @param response
+     */
+    fun addTokenToCookie(name: String, value: String, expiration: Long, response: HttpServletResponse) {
+        val cookie = ResponseCookie.from(name, value)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .sameSite("Strict")
+            .maxAge(expiration)
+            .build()
 
-	/**
-	 * 쿠키에 name의 토큰을 저장하는 메서드
-	 * @param name
-	 * @param value
-	 * @param expiration
-	 * @param response
-	 */
-	public void addTokenToCookie(String name, String value, long expiration, HttpServletResponse response) {
-		ResponseCookie cookie = ResponseCookie.from(name, value)
-			.httpOnly(true)
-			.secure(true)
-			.path("/")
-			.sameSite("Strict")
-			.maxAge(expiration)
-			.build();
+        response.addHeader("Set-Cookie", cookie.toString())
+    }
 
-		response.addHeader("Set-Cookie", cookie.toString());
-	}
-
-	/**
-	 * 쿠키에서 name의 토큰을 받아오는 메서드
-	 * @param name
-	 * @param request
-	 * @return
-	 */
-	public String getTokenFromCookie(String name, HttpServletRequest request) {
-		if (request.getCookies() == null)
-			return null;
-
-		return Arrays.stream(request.getCookies())
-			.filter(cookie -> name.equals(cookie.getName()))
-			.map(Cookie::getValue)
-			.findFirst()
-			.orElse(null);
-	}
-
+    /**
+     * 쿠키에서 name의 토큰을 받아오는 메서드
+     * @param name
+     * @param request
+     * @return
+     */
+    fun getTokenFromCookie(name: String, request: HttpServletRequest): String? {
+        return request.cookies
+            ?.firstOrNull { it.name == name }
+            ?.value
+    }
 }
