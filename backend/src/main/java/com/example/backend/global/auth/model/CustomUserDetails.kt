04ec42bd4d2c7c8a00e5ff610dc20ad1881a35d8
100gammmit <100minha@naver.com>
2025-03-05 12:17:02 +0900

    Refactor: CustomUserDetails 클래스 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/auth/model/CustomUserDetails.kt
+++ backend/src/main/java/com/example/backend/global/auth/model/CustomUserDetails.kt
@@ -1,66 +1,49 @@
-package com.example.backend.global.auth.model;
+package com.example.backend.global.auth.model
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.example.backend.domain.member.dto.MemberInfoDto;
-
-import lombok.RequiredArgsConstructor;
+import com.example.backend.domain.member.dto.MemberInfoDto
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 /**
  * CustomUserDetails
- * <p></p>
  * @author 100minha
  */
-@RequiredArgsConstructor
-public class CustomUserDetails implements UserDetails {
+open class CustomUserDetails(
+    private val memberInfoDto: MemberInfoDto
+) : UserDetails {
 
-	private final MemberInfoDto memberInfoDto;
+    override fun getAuthorities(): Collection<GrantedAuthority?> {
+        return listOf(SimpleGrantedAuthority("USER"))
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority("USER"));
-	}
+    override fun getPassword(): String? {
+        return null
+    }
 
-	@Override
-	public String getPassword() {
-		return null;
-	}
+    override fun getUsername(): String {
+        return memberInfoDto.nickname
+    }
 
-	@Override
-	public String getUsername() {
-		return memberInfoDto.getNickname();
-	}
+    val userId: Long
+        get() = memberInfoDto.id
 
-	public Long getUserId() {
-		return memberInfoDto.getId();
-	}
+    val email: String
+        get() = memberInfoDto.email
 
-	public String getEmail() {
-		return memberInfoDto.getEmail();
-	}
+    override fun isAccountNonExpired(): Boolean {
+        return super.isAccountNonExpired()
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return UserDetails.super.isAccountNonExpired();
-	}
+    override fun isAccountNonLocked(): Boolean {
+        return super.isAccountNonLocked()
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return UserDetails.super.isAccountNonLocked();
-	}
+    override fun isCredentialsNonExpired(): Boolean {
+        return super.isCredentialsNonExpired()
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return UserDetails.super.isCredentialsNonExpired();
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return UserDetails.super.isEnabled();
-	}
+    override fun isEnabled(): Boolean {
+        return super.isEnabled()
+    }
 }
