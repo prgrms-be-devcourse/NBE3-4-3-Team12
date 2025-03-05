ea68e8e0a870e116217257e16b418f830b58ebd4
100gammmit <100minha@naver.com>
2025-03-05 21:44:12 +0900

    Refactor: KakaoTokenResponseDto 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/auth/kakao/dto/KakaoTokenResponseDto.kt
+++ backend/src/main/java/com/example/backend/global/auth/kakao/dto/KakaoTokenResponseDto.kt
@@ -1,24 +1,24 @@
-package com.example.backend.global.auth.kakao.dto;
+package com.example.backend.global.auth.kakao.dto
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * KakaoResponseBodyDto
- * <p></p>
+ *
+ *
  * @author 100minha
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record KakaoTokenResponseDto(
-	@JsonProperty("token_type")
-	String tokenType,
-	@JsonProperty("access_token")
-	String accessToken,
-	@JsonProperty("expires_in")
-	String expiresIn,
-	@JsonProperty("refresh_token")
-	String refreshToken,
-	@JsonProperty("refresh_token_expires_in")
-	String refreshTokenExpiresIn
-) {
-}
+data class KakaoTokenResponseDto(
+    @JsonProperty("token_type")
+    val tokenType: String,
+    @JsonProperty("access_token")
+    val accessToken: String,
+    @JsonProperty("expires_in")
+    val expiresIn: String,
+    @JsonProperty("refresh_token")
+    val refreshToken: String?,
+    @JsonProperty("refresh_token_expires_in")
+    val refreshTokenExpiresIn: String?
+)
