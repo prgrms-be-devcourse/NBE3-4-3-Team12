ee414cc3bf6ccbf1fc3e4fac7031f560a0eed8fb
100gammmit <100minha@naver.com>
2025-03-05 21:44:30 +0900

    Refactor: MemberAuthFilter 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/auth/jwt/MemberAuthFilter.kt
+++ backend/src/main/java/com/example/backend/global/auth/jwt/MemberAuthFilter.kt
@@ -1,32 +1,27 @@
-package com.example.backend.global.auth.jwt;
+package com.example.backend.global.auth.jwt
 
-import java.io.IOException;
-import java.util.Set;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.example.backend.domain.member.dto.MemberTokenReissueDto;
-import com.example.backend.domain.member.exception.MemberException;
-import com.example.backend.global.auth.exception.AuthErrorCode;
-import com.example.backend.global.auth.exception.AuthException;
-import com.example.backend.global.auth.kakao.service.KakaoAuthService;
-import com.example.backend.global.auth.model.CustomUserDetails;
-import com.example.backend.global.auth.service.CookieService;
-import com.example.backend.global.auth.service.CustomUserDetailService;
-import com.example.backend.global.auth.util.JwtUtil;
-import com.example.backend.global.auth.util.TokenProvider;
-import com.example.backend.global.response.ErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import com.example.backend.domain.member.exception.MemberException
+import com.example.backend.global.auth.exception.AuthErrorCode
+import com.example.backend.global.auth.exception.AuthException
+import com.example.backend.global.auth.kakao.service.KakaoAuthService
+import com.example.backend.global.auth.service.CookieService
+import com.example.backend.global.auth.service.CustomUserDetailService
+import com.example.backend.global.auth.util.JwtUtil
+import com.example.backend.global.auth.util.TokenProvider
+import com.example.backend.global.response.ErrorResponse.Companion.of
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
 
 /**
  * MemberAuthFilter
@@ -36,146 +31,158 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class MemberAuthFilter extends OncePerRequestFilter {
+class MemberAuthFilter(
+    private val cookieService: CookieService,
+    private val customUserDetailService: CustomUserDetailService,
+    private val kakaoAuthService: KakaoAuthService,
+    private val jwtUtil: JwtUtil,
+    private val tokenProvider: TokenProvider,
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
 
-	private final CookieService cookieService;
-	private final CustomUserDetailService customUserDetailService;
-	private final KakaoAuthService kakaoAuthService;
-	private final JwtUtil jwtUtil;
-	private final TokenProvider tokenProvider;
+    private val log = LoggerFactory.getLogger(MemberAuthFilter::class.java)
 
-	private final ObjectMapper objectMapper;
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest, response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val accessToken = cookieService.getAccessTokenFromCookie(request)
+        val refreshToken = cookieService.getRefreshTokenFromCookie(request)
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) throws ServletException, IOException {
+        // 엑세스 토큰과 리프레쉬 토큰이 null일 경우 에러 응답
+        // 엑세스 토큰만 null일 경우 리프레쉬 토큰으로 토큰 재발급 후 인증
+        if (accessToken == null) {
+            if (refreshToken == null) {
+                handleAuthError(AuthException(AuthErrorCode.AUTHORIZATION_FAILED), request, response)
+                return
+            }
+            try {
+                val reissuedAccessToken = reissueToken(refreshToken, request, response)
+                setAuthenticationInContext(reissuedAccessToken)
+                filterChain.doFilter(request, response)
+            } catch (e: Exception) {
+                handleAuthError(AuthException(AuthErrorCode.TOKEN_REISSUE_FAILED), request, response)
+            }
+            return
+        }
 
-		final String accessToken = cookieService.getAccessTokenFromCookie(request);
-		final String refreshToken = cookieService.getRefreshTokenFromCookie(request);
+        try {
+            val tokenStatus = jwtUtil.validateToken(accessToken)
 
-		// 엑세스 토큰과 리프레쉬 토큰이 null일 경우 에러 응답
-		// 엑세스 토큰만 null일 경우 리프레쉬 토큰으로 토큰 재발급 후 인증
-		if (accessToken == null) {
-			if (refreshToken == null) {
-				handleAuthError(new AuthException(AuthErrorCode.AUTHORIZATION_FAILED), request, response);
-				return;
-			}
-			String reissuedAccessToken = reissueToken(refreshToken, request, response);
-			setAuthenticationInContext(reissuedAccessToken);
-			filterChain.doFilter(request, response);
-			return;
-		}
+            when (tokenStatus) {
+                TokenStatus.VALID -> setAuthenticationInContext(accessToken)
+                TokenStatus.EXPIRED -> {
+                    log.info("만료된 토큰입니다.")
+                    val reissuedAccessToken = reissueToken(
+                        refreshToken ?: throw AuthException(AuthErrorCode.INVALID_TOKEN),
+                        request, response
+                    )
+                    setAuthenticationInContext(reissuedAccessToken)
+                }
 
-		try {
-			TokenStatus tokenStatus = jwtUtil.validateToken(accessToken);
+                TokenStatus.MALFORMED, TokenStatus.INVALID -> {
+                    log.error("잘못된 형식의 토큰입니다.")
+                    handleAuthError(AuthException(AuthErrorCode.INVALID_TOKEN), request, response)
+                    return
+                }
+            }
+        } catch (e: Exception) {
+            log.error("필터 내부에서 예상치 못한 예외 발생: {}", e.message)
+            handleAuthError(AuthException(AuthErrorCode.AUTHORIZATION_FAILED), request, response)
+            return
+        }
+        filterChain.doFilter(request, response)
+    }
 
-			switch (tokenStatus) {
-				case VALID:
-					setAuthenticationInContext(accessToken);
-					break;
+    /**
+     * 사용자 권한이 필요한 api 경로만 필터링을 하는 메서드
+     * @param request
+     * @return
+     */
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.requestURI
+        val method = request.method
 
-				case EXPIRED:
-					log.info("만료된 토큰입니다.");
-					String reissuedAccessToken = reissueToken(refreshToken, request, response);
-					setAuthenticationInContext(reissuedAccessToken);
-					break;
+        return !(
+                (path == "/groups/member" && method == "GET") ||
+                        (path.startsWith("/groups") && method in setOf("POST", "PUT", "DELETE")) ||
+                        path.startsWith("/members") ||
+                        path.startsWith("/votes") ||
+                        path.startsWith("/voters") ||
+                        path == "/auth/kakao/logout"
+                )
+    }
 
-				case MALFORMED, INVALID:
-					log.error("잘못된 형식의 토큰입니다.");
-					handleAuthError(new AuthException(AuthErrorCode.INVALID_TOKEN), request, response);
-					return;
-			}
-		} catch (Exception e) {
-			log.error("필터 내부에서 예상치 못한 예외 발생: {}", e.getMessage());
-			handleAuthError(new AuthException(AuthErrorCode.AUTHORIZATION_FAILED), request, response);
-			return;
-		}
-		filterChain.doFilter(request, response);
-	}
+    /**
+     * SecurityContext에 인증 정보를 주입하는 메서드
+     * @param accessToken
+     */
+    private fun setAuthenticationInContext(accessToken: String) {
+        val customUserDetails = customUserDetailService.loadUserByAccessToken(accessToken)
+        val authenticationToken = UsernamePasswordAuthenticationToken(
+            customUserDetails, null, customUserDetails.authorities
+        )
 
-	/**
-	 * 사용자 권한이 필요한 api 경로만 필터링을 하는 메서드
-	 * @param request
-	 * @return
-	 */
-	@Override
-	protected boolean shouldNotFilter(HttpServletRequest request) {
+        SecurityContextHolder.getContext().authentication = authenticationToken
+    }
 
-		String path = request.getRequestURI();
-		String method = request.getMethod();
+    /**
+     * 인증에 실패했을 때 로그아웃 처리 및 에러 응답 메서드
+     * @param ex
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    private fun handleAuthError(
+        ex: AuthException,
+        request: HttpServletRequest, response: HttpServletResponse
+    ) {
+        cookieService.clearTokenFromCookie(response)
+        SecurityContextHolder.clearContext()
 
-		return !(
-			(path.equals("/groups/member")) && method.equals("GET") ||
-				(path.startsWith("/groups")) && Set.of("POST", "PUT", "DELETE").contains(method) ||
-				path.startsWith("/members") ||
-				path.startsWith("/votes") ||
-				path.startsWith("/voters") ||
-				path.equals("/auth/kakao/logout")
-		);
-	}
+        response.status = ex.status.value()
+        response.contentType = "application/json;charset=UTF-8"
 
-	/**
-	 * SecurityContext에 인증 정보를 주입하는 메서드
-	 * @param accessToken
-	 */
-	private void setAuthenticationInContext(String accessToken) {
+        val errorResponse = of(ex.message, ex.code, request.requestURI)
+        objectMapper.writeValue(response.writer, errorResponse)
+    }
 
-		CustomUserDetails customUserDetails = customUserDetailService.loadUserByAccessToken(accessToken);
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-			customUserDetails, null, customUserDetails.getAuthorities()
-		);
+    /**
+     * 토큰 재발급 및 쿠키에 토큰 정보 저장하는 메서드
+     * @param refreshToken
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    private fun reissueToken(
+        refreshToken: String,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): String {
+        try {
+            val memberTokenReissueDto = kakaoAuthService.reissueTokens(refreshToken)
 
-		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-	}
+            val reissuedAccessToken = tokenProvider.generateMemberAccessToken(
+                memberTokenReissueDto.id, memberTokenReissueDto.nickname,
+                memberTokenReissueDto.email
+            )
 
-	/**
-	 * 인증에 실패했을 때 로그아웃 처리 및 에러 응답 메서드
-	 * @param ex
-	 * @param request
-	 * @param response
-	 * @throws IOException
-	 */
-	private void handleAuthError(AuthException ex,
-		HttpServletRequest request, HttpServletResponse response) throws IOException {
+            cookieService.addAccessTokenToCookie(reissuedAccessToken, response)
+            cookieService.addRefreshTokenToCookie(memberTokenReissueDto.refreshToken, response)
 
-		cookieService.clearTokenFromCookie(response);
-		SecurityContextHolder.clearContext();
-
-		response.setStatus(ex.getStatus().value());
-		response.setContentType("application/json;charset=UTF-8");
-
-		ErrorResponse errorResponse = ErrorResponse.of(ex.getMessage(), ex.getCode(), request.getRequestURI());
-		objectMapper.writeValue(response.getWriter(), errorResponse);
-	}
-
-	/**
-	 * 토큰 재발급 및 쿠키에 토큰 정보 저장하는 메서드
-	 * @param refreshToken
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 */
-	private String reissueToken(String refreshToken, HttpServletRequest request, HttpServletResponse response) throws
-		IOException {
-		try {
-			MemberTokenReissueDto memberTokenReissueDto = kakaoAuthService.reissueTokens(refreshToken);
-
-			String reissuedAccessToken = tokenProvider.generateMemberAccessToken(
-				memberTokenReissueDto.getId(), memberTokenReissueDto.getNickname(),
-				memberTokenReissueDto.getEmail());
-
-			cookieService.addAccessTokenToCookie(reissuedAccessToken, response);
-			cookieService.addRefreshTokenToCookie(memberTokenReissueDto.getRefreshToken(), response);
-
-			return reissuedAccessToken;
-		} catch (MemberException e) {
-			log.error("유효하지 않은 인증정보입니다.");
-			handleAuthError(new AuthException(AuthErrorCode.INVALID_TOKEN), request, response);
-		} catch (Exception e) {
-			log.error("토큰 갱신 중 오류 발생: {}", e.getMessage());
-			handleAuthError(new AuthException(AuthErrorCode.TOKEN_REISSUE_FAILED), request, response);
-		}
-		return null;
-	}
+            return reissuedAccessToken
+        } catch (e: MemberException) {
+            log.error("유효하지 않은 인증정보입니다.")
+            handleAuthError(AuthException(AuthErrorCode.INVALID_TOKEN), request, response)
+            throw e
+        } catch (e: Exception) {
+            log.error("토큰 갱신 중 오류 발생: {}", e.message)
+            handleAuthError(AuthException(AuthErrorCode.TOKEN_REISSUE_FAILED), request, response)
+            throw e
+        }
+    }
 }
