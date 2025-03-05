800dc968d84ad5ae8191f8a8b6b44751c510646b
100gammmit <100minha@naver.com>
2025-03-05 21:39:17 +0900

    Refactor: CustomUserDetailService 클래스 코틀린 리팩토링 (#3)

--- backend/src/main/java/com/example/backend/global/auth/service/CustomUserDetailService.kt
+++ backend/src/main/java/com/example/backend/global/auth/service/CustomUserDetailService.kt
@@ -1,38 +1,30 @@
-package com.example.backend.global.auth.service;
+package com.example.backend.global.auth.service
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import com.example.backend.domain.member.dto.MemberInfoDto;
-import com.example.backend.domain.member.entity.Member;
-import com.example.backend.domain.member.service.MemberService;
-import com.example.backend.global.auth.model.CustomUserDetails;
-import com.example.backend.global.auth.util.JwtUtil;
-
-import lombok.RequiredArgsConstructor;
+import com.example.backend.domain.member.dto.MemberInfoDto
+import com.example.backend.domain.member.service.MemberService
+import com.example.backend.global.auth.model.CustomUserDetails
+import com.example.backend.global.auth.util.JwtUtil
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
 
 /**
  * CustomUserDetailService
- * <p></p>
  * @author 100minha
  */
 @Service
-@RequiredArgsConstructor
-public class CustomUserDetailService implements UserDetailsService {
+class CustomUserDetailService(
+    private val memberService: MemberService,
+    private val jwtUtil: JwtUtil
+) : UserDetailsService {
 
-	private final MemberService memberService;
-	private final JwtUtil jwtUtil;
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): CustomUserDetails {
+        val member = memberService.findById(username.toLong())
+        return CustomUserDetails(MemberInfoDto.of(member))
+    }
 
-	@Override
-	public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-		Member member = memberService.findById(Long.valueOf(username));
-		return new CustomUserDetails(MemberInfoDto.Companion.of(member));
-	}
-
-	public CustomUserDetails loadUserByAccessToken(String accessToken) {
-
-		return new CustomUserDetails(jwtUtil.getMemberInfoDto(accessToken));
-	}
+    fun loadUserByAccessToken(accessToken: String): CustomUserDetails {
+        return CustomUserDetails(jwtUtil.getMemberInfoDto(accessToken))
+    }
 }
