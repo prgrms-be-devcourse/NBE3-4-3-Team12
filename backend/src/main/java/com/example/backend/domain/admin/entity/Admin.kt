680a0517a90aa4b0f42a82cd37792d7227eb8052
gorhkd <gorhkd223@gmail.com>
2025-03-05 17:28:49 +0900

    Feat: PR 피드백 반영

--- backend/src/main/java/com/example/backend/domain/admin/entity/Admin.kt
+++ backend/src/main/java/com/example/backend/domain/admin/entity/Admin.kt
@@ -9,23 +9,20 @@ class Admin (
     @Column(unique = true)
     val adminName: String,
     val password: String,
-
-    @Enumerated(EnumType.STRING)
-    val role: Role,
-
-    @Column
-    var refreshToken: String? = null,
-
-    @Column
-    var refreshTokenExpiryDate: LocalDateTime? = null
 ) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
-    constructor(adminName: String, password: String) : this(
-        adminName, password, Role.ADMIN)
+    @Enumerated(EnumType.STRING)
+    val role: Role = Role.ADMIN
+
+    @Column
+    var refreshToken: String? = null
+
+    @Column
+    var refreshTokenExpiryDate: LocalDateTime? = null
 
     fun setRefreshToken(refreshToken: String?, expiryDate: LocalDateTime?) {
         this.refreshToken = refreshToken
