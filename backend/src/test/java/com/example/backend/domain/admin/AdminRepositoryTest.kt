ba1d6314b1ce0b09023571a110580cb7242457ae
gorhkd <gorhkd223@gmail.com>
2025-03-07 17:56:02 +0900

    Feat: 리프레시토큰 및 Redis 테스트 작성

--- backend/src/test/java/com/example/backend/domain/admin/AdminRepositoryTest.kt
+++ backend/src/test/java/com/example/backend/domain/admin/AdminRepositoryTest.kt
@@ -12,12 +12,9 @@ import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
-import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @DataJpaTest
 @ActiveProfiles("test")
-@Transactional
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AdminRepositoryTest(
@@ -25,12 +22,10 @@ class AdminRepositoryTest(
 ) {
 
     private lateinit var admin: Admin
-    private val refeshToken = "468cb628-5f71-4e34-89fb-7e5b6ce7575b"
 
         @BeforeAll
         fun init() {
             admin = Admin("admin", "\$2a\$12\$wS8w9vGzZ345XlGazbp8mekCkPyKoPFbky96pr0EqW.6I0Xtdt.YO")
-            admin.setRefreshToken("468cb628-5f71-4e34-89fb-7e5b6ce7575b", LocalDateTime.now())
             adminRepository.save(admin)
         }
 
@@ -40,7 +35,6 @@ class AdminRepositoryTest(
         val admin = adminRepository.findById(1)
             .orElseThrow{AdminException(AdminErrorCode.NOT_FOUND_ADMIN)}
         assertThat(admin.adminName).isEqualTo("admin")
-        assertThat(admin.refreshToken).isEqualTo(refeshToken)
     }
 
     @Test
@@ -48,28 +42,12 @@ class AdminRepositoryTest(
     fun findByAdminName() {
         val admin = adminRepository.findByAdminName("admin")
         assertThat(admin!!.adminName).isEqualTo("admin")
-        assertThat(admin.refreshToken).isEqualTo(refeshToken)
     }
 
     @Test
     @DisplayName("findByAdminName() 실패 테스트")
     fun findByAdminName_fail() {
         val admin = adminRepository.findByAdminName("not_exist")
-        assertThat(admin).isNull()
-    }
-
-    @Test
-    @DisplayName("findByRefreshToken 검증")
-    fun findByRefreshToken() {
-        val admin = adminRepository.findByRefreshToken(refeshToken)
-        assertThat(admin!!.adminName).isEqualTo("admin")
-        assertThat(admin.refreshToken).isEqualTo(refeshToken)
-    }
-
-    @Test
-    @DisplayName("findByRefreshToken 실패 테스트")
-    fun findByRefreshToke_fail() {
-        val admin = adminRepository.findByRefreshToken("")
         assertThat(admin).isNull()
     }
 }
