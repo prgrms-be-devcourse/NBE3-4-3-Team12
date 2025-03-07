3f099e33229088d3d8d7763cf64c850d6a183ee5
100gammmit <100minha@naver.com>
2025-03-07 12:28:28 +0900

    Refactor: RedisDao 작성 (#34)
    - redis db에 접근해서 쿼리를 실행시키는 repository 클래스

---
+++ backend/src/main/java/com/example/backend/global/redis/dao/RedisDao.kt
@@ -0,0 +1,48 @@
+package com.example.backend.global.redis.dao
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * RedisDao
+ * <p></p>
+ * @author 100minha
+ */
+@Repository
+class RedisDao(
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+
+    // 데이터 저장 (만료 시간 설정 없이)
+    fun save(key: String, value: String) {
+        redisTemplate.opsForValue().set(key, value)
+    }
+
+    // 데이터 저장 (만료 시간 설정 포함)
+    fun save(key: String, value: String, expirationTime: Long) {
+        redisTemplate.opsForValue().set(key, value, expirationTime, TimeUnit.SECONDS)
+    }
+
+    // 데이터 조회
+    fun get(key: String): String? {
+        return redisTemplate.opsForValue().get(key)
+    }
+
+    // 데이터 존재 여부 확인
+    fun exists(key: String): Boolean {
+        return redisTemplate.hasKey(key)
+    }
+
+    // 데이터 삭제
+    fun delete(key: String) {
+        redisTemplate.delete(key)
+    }
+
+    // 만료 시간 갱신 (시간 단위는 초)
+    fun setExpiration(key: String, expirationTimeInSeconds: Long) {
+        redisTemplate.expire(key, expirationTimeInSeconds, TimeUnit.SECONDS)
+    }
+
+}
