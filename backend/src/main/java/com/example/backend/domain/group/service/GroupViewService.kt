caa1f5febcb5a952691e6b1f7a548e8d554a74e8
quiemew0 <quiemew@gmail.com>
2025-03-10 15:28:19 +0900

    feat : 프론트에서 조회수 표시, 로그

--- backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
+++ backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
@@ -1,9 +1,12 @@
 package com.example.backend.domain.group.service
 
+import lombok.extern.slf4j.Slf4j
+import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
 
+@Slf4j
 @Service
 class GroupViewService(private val redisTemplate: StringRedisTemplate) {
 
@@ -14,6 +17,8 @@ class GroupViewService(private val redisTemplate: StringRedisTemplate) {
         val groupKey = "$viewKeyPrefix$groupId"
         val userViewKey = "$userViewedPrefix$groupId:$userId"
 
+        log.info("groupkey는 : $groupKey")
+        log.info("userKey는 : $userViewKey")
         if (redisTemplate.opsForValue().get(userViewKey) == null) {
             redisTemplate.opsForValue().increment(groupKey)
             redisTemplate.opsForValue().set(userViewKey, "viewed", 24, TimeUnit.HOURS) // 24시간 동안만 조회한 것으로 처리
