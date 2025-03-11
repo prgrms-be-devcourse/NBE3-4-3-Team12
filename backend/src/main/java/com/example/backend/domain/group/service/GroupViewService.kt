e04ca6a24f0210c23c2c92c9e602b7692fff08d4
quiemew0 <quiemew@gmail.com>
2025-03-11 10:04:41 +0900

    refactor : 레디스 사용 관련 리팩토링

--- backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
+++ backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
@@ -1,14 +1,13 @@
 package com.example.backend.domain.group.service
 
+import com.example.backend.global.redis.service.RedisService
 import lombok.extern.slf4j.Slf4j
 import org.hibernate.query.sqm.tree.SqmNode.log
-import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
-import java.util.concurrent.TimeUnit
 
 @Slf4j
 @Service
-class GroupViewService(private val redisTemplate: StringRedisTemplate) {
+class GroupViewService(private val redisService: RedisService) {
 
     private val viewKeyPrefix = "group:views:"
     private val userViewedPrefix = "group:user:viewed:"
@@ -19,14 +18,16 @@ class GroupViewService(private val redisTemplate: StringRedisTemplate) {
 
         log.info("groupkey는 : $groupKey")
         log.info("userKey는 : $userViewKey")
-        if (redisTemplate.opsForValue().get(userViewKey) == null) {
-            redisTemplate.opsForValue().increment(groupKey)
-            redisTemplate.opsForValue().set(userViewKey, "viewed", 24, TimeUnit.HOURS) // 24시간 동안만 조회한 것으로 처리
+        if (!redisService.isUserViewed(groupId, userId)) {
+            val newViewCount = redisService.incrementViewCount(groupId)
+            redisService.markUserAsViewed(groupId, userId)
+
+            log.info("New view count for group $groupId: $newViewCount")
         }
     }
 
     fun getViewCount(groupId: Long): Long {
-        val key = "$viewKeyPrefix$groupId"
-        return redisTemplate.opsForValue().get(key)?.toLong() ?: 0
+        val groupKey = "$viewKeyPrefix$groupId"
+        return redisService.getViewCount(groupId)
     }
 }
