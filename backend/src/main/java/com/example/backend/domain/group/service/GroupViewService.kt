792f0e77a0d0c211bdc4a2c533a80ca601b6cba4
quiemew0 <quiemew@gmail.com>
2025-03-11 16:32:01 +0900

    Feat : 인기 게시글 매주 일요일 캐시 삭제 및 저장

--- backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
+++ backend/src/main/java/com/example/backend/domain/group/service/GroupViewService.kt
@@ -1,33 +1,17 @@
 package com.example.backend.domain.group.service
 
 import com.example.backend.global.redis.service.RedisService
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class GroupViewService(private val redisService: RedisService) {
-
-    private val log = LoggerFactory.getLogger(GroupViewService::class.java)
-
-    private val viewKeyPrefix = "group:views:"
-    private val userViewedPrefix = "group:user:viewed:"
-
+class GroupViewService(
+    private val redisService: RedisService) {
     fun incrementViewCount(groupId: Long, userId: Long) {
-        val groupKey = "$viewKeyPrefix$groupId"
-        val userViewKey = "$userViewedPrefix$groupId:$userId"
-
-        log.info("groupkey는 : $groupKey")
-        log.info("userKey는 : $userViewKey")
         if (!redisService.isUserViewed(groupId, userId)) {
-            val newViewCount = redisService.incrementViewCount(groupId)
             redisService.markUserAsViewed(groupId, userId)
-
-            log.info("New view count for group $groupId: $newViewCount")
         }
     }
-
     fun getViewCount(groupId: Long): Long {
-        val groupKey = "$viewKeyPrefix$groupId"
         return redisService.getViewCount(groupId)
     }
 }
