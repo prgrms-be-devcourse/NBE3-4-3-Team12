a48bb5a836429d8a044893bbf90ab37caa3fc97c
quiemew0 <quiemew@gmail.com>
2025-03-11 17:07:23 +0900

    Refactor : 안쓰는 import문 제거

--- backend/src/main/java/com/example/backend/domain/group/controller/GroupController.kt
+++ backend/src/main/java/com/example/backend/domain/group/controller/GroupController.kt
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/groups")
 class GroupController(
